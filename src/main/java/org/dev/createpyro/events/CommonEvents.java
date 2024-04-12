29535fe00f08333012044e9b1169340b221ab331
Tejty <damian.bonus@gmail.com>
2024-04-12 13:56:21 +0200

    Better explosion
    Added randomness and decreased velocity of the falling blocks
    Fixed rendering issue
    
    Broken state for the falling blocks. Need to fix

--- src/main/java/org/dev/createpyro/events/CommonEvents.java
+++ src/main/java/org/dev/createpyro/events/CommonEvents.java
@@ -1,41 +1,42 @@
 package org.dev.createpyro.events;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Vec3i;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.ParticleUtils;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FallingBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.dev.createpyro.Pyro;
-import org.dev.createpyro.block.GunPowderWireBlock;
 import org.dev.createpyro.registry.PyroBlocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Pyro.ID)
 public class CommonEvents {
     public static final Logger LOGGER = LoggerFactory.getLogger(Pyro.NAME);
+    private static Random random = new Random();
 
     @SubscribeEvent
     public static InteractionResult gunPowderRightClick(PlayerInteractEvent.RightClickBlock event) {
@@ -65,7 +66,6 @@ public class CommonEvents {
 
     @SubscribeEvent
     public static void onExplosion(ExplosionEvent.Detonate event) {
-        //event.setCanceled(true);
         Level level = event.getLevel();
         Explosion explosion = event.getExplosion();
         List<BlockPos> blocks = event.getAffectedBlocks();
@@ -74,19 +74,51 @@ public class CommonEvents {
 
         for (BlockPos affectedPos : blocks){
             Vec3 relative = new Vec3(
-                    (affectedPos.getX() - pos.x) * 0.5F,
-                    (affectedPos.getY() - pos.y) * 0.5F + 1,
-                    (affectedPos.getZ() - pos.z) * 0.5F
+                    (affectedPos.getX() - pos.x + 0.5) * 0.3F + random.nextDouble(-0.1, 0.1),
+                    (affectedPos.getY() - pos.y + 0.5) * 0.3F + random.nextDouble(-0.1, 0.1) + 0.3,
+                    (affectedPos.getZ() - pos.z + 0.5) * 0.3F + random.nextDouble(-0.1, 0.1)
                     );
+            BlockState state = level.getBlockState(affectedPos);
+            FallingBlockEntity entity = fall(
+                    level,
+                    new Vec3(
+                            affectedPos.getX(),
+                            affectedPos.getY(),
+                            affectedPos.getZ()
+                    ),
+                    state
+            );
+
+            if (entity == null){
+                continue;
+            }
 
 
-            FallingBlockEntity entity = FallingBlockEntity.fall(level, affectedPos, level.getBlockState(affectedPos));
             entity.setDeltaMovement(
                     relative
             );
+            level.setBlock(affectedPos, Blocks.AIR.defaultBlockState(), 3);
+            level.addFreshEntity(entity);
+        }
+    }
+
+    private static FallingBlockEntity fall(Level level, Vec3 pos, BlockState state){
+        if (state.is(Blocks.AIR)){
+            return null;
         }
 
-
-        level.setBlock(blockPos, Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
+        FallingBlockEntity entity = EntityType.FALLING_BLOCK.create(level);
+        entity.blocksBuilding = true;
+        entity.setPos(new Vec3(
+                pos.x() + 0.5,
+                pos.y(),
+                pos.z() + 0.5
+        ));
+        entity.setDeltaMovement(Vec3.ZERO);
+        entity.xo = pos.x();
+        entity.yo = pos.y();
+        entity.zo = pos.z();
+        entity.setStartPos(entity.blockPosition());
+        return entity;
     }
 }
